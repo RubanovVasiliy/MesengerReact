8f68e105f0f44c3512896cd15374cd36aee14a51
Vasiliy <rubanovas2005@gmail.com>
2021-06-21 19:38:29 +0700

    Add jsx func Technologies

---
+++ src/Technologies.js
@@ -0,0 +1,11 @@
+function Technologies() {
+    return (
+        <lu>
+            <li>css</li>
+            <li>js</li>
+            <li>html</li>
+        </lu>
+    );
+}
+
+export default Technologies;
