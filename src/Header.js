41e4e427cff19a3dccd01aa715d8c0f5fe22318f
Vasiliy <rubanovas2005@gmail.com>
2021-06-21 19:43:18 +0700

    Add jsx func Header

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div >
+            <a href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#about">About</a>
+        </div>
+    );
+}
+
+export default Header;
