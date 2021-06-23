fc9153ae2442e24b78f5b44d5251542f14e8141a
Vasiliy <rubanovas2005@gmail.com>
2021-06-23 19:52:35 +0700

    Add routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {Route, BrowserRouter} from 'react-router-dom'
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 function App() {
     return (
@@ -14,6 +17,9 @@ function App() {
                 <div className='app-wrapper-content'>
                     <Route path='/profile' component={Profile}/>
                     <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/news' component={News}/>
+                    <Route path='/music' component={Music}/>
+                    <Route path='/settings' component={Settings}/>
                 </div>
             </div>
         </BrowserRouter>
