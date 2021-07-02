9e05eb5283836e0b4c11a17e8734f54d0aa6cd09
Vasiliy <rubanovas2005@gmail.com>
2021-07-02 23:48:33 +0700

    Add auth

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import {Route} from 'react-router-dom'
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 function App(props) {
 
     return (
         <div className='app-wrapper'>
-            <Header/>
-            <Navbar store={props.store}/>
-            <div className='app-wrapper-content'>
-                <Route path='/profile' render={() => <Profile/>}/>
-                <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                <Route path='/news' render={() => <News/>}/>
-                <Route path='/music' render={() => <Music/>}/>
-                <Route path='/settings' render={() => <Settings/>}/>
-                <Route path='/users' render={() => <UsersContainer />}/>
-                {/*<Route path='/friends' render={() => <Friends store={props.store}/>}/>*/}
+            <HeaderContainer className='app-wrapper-header'/>
+                <Navbar store={props.store}/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/settings' render={() => <Settings/>}/>
+                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    {/*<Route path='/friends' render={() => <Friends store={props.store}/>}/>*/}
+                </div>
 
-            </div>
         </div>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import logo from "../../images/logo.png";
 import classes from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-function Header() {
+function Header(props) {
     return (
         <header className={classes.header}>
             <img src={logo} alt=''/>
+            <div className={classes.loginBlock}>
+                {props.isAuth
+                    ? <div>{props.login}</div>
+                    : <NavLink to={'/login'}>Login</NavLink>
+                }
+            </div>
         </header>
     );
 }
