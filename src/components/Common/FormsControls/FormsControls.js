15d1fe93d694d50eaf972c91efa63238931d341d
Vasiliy <rubanovas2005@gmail.com>
2021-07-25 13:09:43 +0700

    Refactored code

--- src/components/Common/FormsControls/FormsControls.js
+++ src/components/Common/FormsControls/FormsControls.js
@@ -1,14 +1,16 @@
- import styles from './FormsControls.module.css'
+import styles from './FormsControls.module.css'
+import {Field} from "redux-form";
+import React from "react";
 
 
-export function FormControl({input, meta, child, ...props}) {
-    const hasError = meta.touched && meta.error
+export function FormControl({meta: {touched, error}, children}) {
+    const hasError = touched && error
     return (
         <div className={styles.formControl + ' ' + (hasError ? styles.error : '')}>
             <div>
-                {props.children}
+                {children}
             </div>
-            {hasError && <span>{meta.error}</span>}
+            {hasError && <span>{error}</span>}
         </div>
     )
 }
@@ -22,3 +24,13 @@ export function Input(props) {
     const {input, meta, child, ...restProps} = props
     return <FormControl {...props}> <input{...input}{...restProps} /></FormControl>
 }
+
+export const createField = (placeholder, name, validators, component, props = {}, text = '') => (
+    <div>
+        <Field placeholder={placeholder} name={name}
+               component={component}
+               validate={validators}
+               {...props}
+        />{text}
+    </div>
+)
